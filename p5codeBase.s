2866e8d17d16a08a248649acc8323fd72e7088fa
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2018-04-25 23:11:56 -0700

    Update p5codeBase.s

--- p5codeBase.s
+++ p5codeBase.s
@@ -21,13 +21,53 @@ getDigit:
 # digit --> $s1
 # 
 ##
-sumOfDoubleEvenPlace:
+
+.text
+.globl main
 
 main:
-  li $s0, 89744563  # int test1 = 89744563;
-  li $s1, 98756421  # int test2 = 98756421;
-  li $s2, 0         # int result1 = 0;
-  li $s3, 0         # int result2 = 0;
+li $s0,89744563
+li $s1,98756421
+li $s2,0
+li $s3,0
+move $t1,$s0
+jal sumOfDoubleEvenPlace
+move $s2,$t0
+move $t1,$s1
+jal sumOfDoubleEvenPlace
+move $s3,$t0
+
+sumOfDoubleEvenPlace:
+
+li $t0,0
+li $a1,10
+li $a2,100
+div $t1,$a1
+mflo $t1
+while:
+div $t1,$a1
+mfhi $t2
+j getdigit
+return:
+div $t1,$a2
+mflo $t1
+bgt $t1,0,while
+jr $ra
+  
+getdigit:
+mul $t3,$t2,2
+bgt $t3,10,else
+add $t0,$t0,$t3
+j return
+else:
+div $t3,$a1
+mfhi $t4
+add $t0,$t0,$t4
+div $t3,$a1
+mflo $t4
+add $t0,$t0,$t4
+j return
+
 
 
   # code for first function call
