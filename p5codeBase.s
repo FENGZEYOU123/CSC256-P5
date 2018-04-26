a29873971a77f76a3d523f0129d0ae18a850abaa
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2018-04-26 12:37:33 -0700

    Update p5codeBase.s

--- p5codeBase.s
+++ p5codeBase.s
@@ -20,17 +20,6 @@
 #
 # #
 
-main:
-li $s0,89744563
-li $s1,98756421
-li $s2,0
-li $s3,0
-move $t1,$s0
-jal sumOfDoubleEvenPlace
-move $s2,$t0
-move $t1,$s1
-jal sumOfDoubleEvenPlace
-move $s3,$t0
 
 
 
@@ -64,6 +53,19 @@ div $t3,$a1
 mflo $t4
 add $t0,$t0,$t4
 j return
+
+main:
+li $s0,89744563
+li $s1,98756421
+li $s2,0
+li $s3,0
+move $t1,$s0
+jal sumOfDoubleEvenPlace
+move $s2,$t0
+move $t1,$s1
+jal sumOfDoubleEvenPlace
+move $s3,$t0
+
 
   # code for first function call
 
